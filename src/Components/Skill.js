0fca64a92728c06bb93d626692791f466848bbb7
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-10-17 23:43:19 +0530

    feat : add skill component

---
+++ src/Components/Skill.js
@@ -0,0 +1,23 @@
+import { Box, Flex, HStack, Icon, Text } from "@chakra-ui/react";
+import React from "react";
+import { FaHtml5, FaCss3Alt, FaReact, FaJs, FaNpm } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+const Skill = () => {
+  return (
+    <Box m={4}>
+      <Text textAlign={"center"} fontWeight="bold" m={4} fontSize="4xl">
+        Skills
+      </Text>
+      <HStack wrap={"wrap"}>
+        <Icon as={FaHtml5} w={100} h={100} />
+        <Icon as={FaCss3Alt} w={100} h={100} />
+        <Icon as={FaJs} w={100} h={100} />
+        <Icon as={FaReact} w={100} h={100} />
+        <Icon as={SiRedux} w={100} h={100} />
+        <Icon as={FaNpm} w={100} h={100} />
+      </HStack>
+    </Box>
+  );
+};
+
+export { Skill };
