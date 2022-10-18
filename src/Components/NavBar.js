98a0bbe59f92073c1586e5ed3dea14099d1edbc3
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-10-18 22:03:02 +0530

    fix : social links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import {
   useColorMode,
   IconButton,
   Spacer,
+  Link,
 } from "@chakra-ui/react";
 import {
   FaSun,
@@ -16,30 +17,40 @@ import {
 const NavBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
+  // https://github.com/ajay-mandaviya
   return (
     <Flex width={"100%"} alignItems={"center"}>
       <Heading size={"md"} fontWeight={"semibold"} color="cyan.400">
         Ajay
       </Heading>
       <Spacer />
-      <IconButton
-        aria-label="linkdin"
-        isRound={true}
-        icon={<FaLinkedinIn />}
-        marginRight={4}
-      />
-      <IconButton
-        aria-label="github"
-        isRound={true}
-        icon={<FaGithub />}
-        marginRight={4}
-      />
-      <IconButton
-        aria-label="twitter"
-        isRound={true}
-        icon={<FaTwitter />}
-        marginRight={4}
-      />
+      <Link
+        href="https://www.linkedin.com/in/ajay-mandaviya-222900191/"
+        isExternal
+      >
+        <IconButton
+          aria-label="linkdin"
+          isRound={true}
+          icon={<FaLinkedinIn />}
+          marginRight={4}
+        />
+      </Link>
+      <Link isExternal href="https://github.com/ajay-mandaviya">
+        <IconButton
+          aria-label="github"
+          isRound={true}
+          icon={<FaGithub />}
+          marginRight={4}
+        />
+      </Link>
+      <Link isExternal href="https://twitter.com/AjayMandaviya">
+        <IconButton
+          aria-label="twitter"
+          isRound={true}
+          icon={<FaTwitter />}
+          marginRight={4}
+        />
+      </Link>
       <IconButton
         aria-label="theme"
         isRound={true}
